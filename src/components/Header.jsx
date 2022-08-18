9dceab6386e14ab24d90a96f4d31e27c3b545a45
Vladislav Karas <dncmorgan@yandex.ru>
2022-08-18 21:39:05 +0300

    Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://cdn.icon-icons.com/icons2/3317/PNG/512/deep_anglerfish_fish_angler_animal_icon_209487.png' alt='fish logo' />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://cdn.fishki.net/upload/post/2020/01/10/3196335/103039-plashh-pejzazhi-gor-more-ozero-pribrezhnyye-i-okeanicheskiye-relyefy-2560x1440.jpg' alt='Ocean' />
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            my posts
+        </div>
+        <div>
+            new posts
+        </div>
+        <div>
+            post 1
+        </div>
+        <div>
+            post 2
+        </div>
+    </div>
+}
+
+export default Profile;
