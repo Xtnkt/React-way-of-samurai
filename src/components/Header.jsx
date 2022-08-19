b08af841a0a190dfd40be526bc3a46c4a939e838
Vladislav Karas <dncmorgan@yandex.ru>
2022-08-19 21:30:04 +0300

    css-modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={classes.header}>
         <img src='https://cdn.icon-icons.com/icons2/3317/PNG/512/deep_anglerfish_fish_angler_animal_icon_209487.png' alt='fish logo' />
     </header>
 }
